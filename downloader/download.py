e5bc4476ce6e6b012b4dd26aa54e5cee571719bf
Eugene Feshchenko <eugene@cybeready.com>
2020-01-18 21:53:22 +0200

    Update

--- downloader/download.py
+++ downloader/download.py
@@ -1,21 +1,28 @@
+import argparse
 import codecs
 import json
 import logging
 import os
 import time
-from urllib.request import urlretrieve, FancyURLopener
+from urllib.request import urlretrieve
 
 logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 
 class Downloader:
-    def __init__(self, batch_size=30):
-        self.batch_size = batch_size
+    def __init__(self):
         with open('input/data.json', 'r') as f:
             data = f.read()
             data = json.loads(data)
             data.sort(key=lambda x: x['number'], reverse=True)
         self.data = data
+
+        parser = argparse.ArgumentParser(description='Скачивает файлы с сайта MDS.')
+        parser.add_argument(
+            '-n', metavar='Number', default=30, type=int, required=False, help='количество файлов(30 по умолчанию)'
+        )
+        self.args = parser.parse_args()
+        self.batch_size = self.args.n
 
     @staticmethod
     def reporthook(count: int, block_size: int, total_size: int) -> None:
@@ -32,43 +39,40 @@ class Downloader:
         if total_size > 0:
             percent = int(count * block_size * 100 / total_size)
             print(f"... {percent}%, {progress_size:.0f} MB, {duration:.0f} секунд", end='\r')
-            if percent >= 100:
-                print('\n')
         else:
             print(f"... {progress_size:.2f} MB, {duration:.0f} секунд", end='\r')
 
+    def save_data(self) -> None:
+        with codecs.open('input/data.json', 'w+', encoding='utf-8') as f:
+            f.write(json.dumps(self.data, ensure_ascii=False, indent=2))
+
     def download_batch(self) -> None:
         downloaded_counter = 0
-        item_idx = 0
-        data = self.data
-        while downloaded_counter < self.batch_size and item_idx <= len(self.data):
-            item = data[item_idx]
-            if item['downloaded'] in [False, 'err']:
-                filename = f"{item['number']}.{item['author']} - {item['name']}.mp3".replace('/', '_')
-                logging.info(filename)
-                links = item['links'].split(' || ')
-                for index, link in enumerate(links):
-                    logging.info(f"Ссылка {index + 1} из {len(links)}")
-                    try:
-                        urlretrieve(link, f'output/{filename}', self.reporthook)
-                        item['downloaded'] = True
-                        downloaded_counter += 1
-                        break
-                    except (Exception, KeyboardInterrupt) as e:
-                        logging.info(f'Отмена ({e})')
-                        continue
-                else:
-                    data[item_idx]['downloaded'] = 'err'
 
-                with codecs.open('input/data.json', 'w+', encoding='utf-8') as f:
-                    f.write(json.dumps(data, ensure_ascii=False, indent=2))
+        for item in filter(lambda x: x['downloaded'] != True, self.data):
+            filename = f"{item['number']}.{item['author']} - {item['name']}.mp3".replace('/', '_')
+            logging.info(filename)
+            links = item['links'].split(' || ')
+            for index, link in enumerate(links):
+                logging.info(f"Ссылка {index + 1} из {len(links)}")
+                try:
+                    urlretrieve(link, f'output/{filename}', self.reporthook)
+                    item['downloaded'] = True
+                    downloaded_counter += 1
+                    break
+                except (Exception, KeyboardInterrupt) as e:
+                    logging.info(f'Отмена ({e})')
+                    continue
+            else:
+                item['downloaded'] = 'err'
 
-            item_idx += 1
-        else:
-            logging.info('')
-            logging.info('Готово!')
+            self.save_data()
+            logging.info('_' * 50)
 
-        self.cleanup()
+            if downloaded_counter >= self.batch_size:
+                logging.info(f'Готово! {downloaded_counter}')
+                self.cleanup()
+                break
 
     @staticmethod
     def cleanup() -> None:
